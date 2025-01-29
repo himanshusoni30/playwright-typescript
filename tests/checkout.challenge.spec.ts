253c9540bd0cb55b5a51b8a81551284c4d6da901
Himanshu Soni <eng.sonihimanshu@gmail.com>
2025-01-29 19:51:55 +0530

    skipping visual validation

--- tests/checkout.challenge.spec.ts
+++ tests/checkout.challenge.spec.ts
@@ -10,7 +10,7 @@ test.describe('Verify screenshots -> add item to cart and confirm payment', () =
         await page.goto(String(process.env.UI_BASE_URL));
     });
 
-    test('Search the product "Thor Hammer" and verify the result in grid', async ({page}) => {
+    test('Search the product "Thor Hammer" and verify the result in grid', async ({ page }) => {
         // await page.goto("https://practicesoftwaretesting.com");
         const products = page.locator('.col-md-9');
         await page.getByPlaceholder('Search').fill('Thor Hammer');
@@ -18,7 +18,10 @@ test.describe('Verify screenshots -> add item to cart and confirm payment', () =
         await expect(products.getByRole('link')).toHaveCount(1);
         await expect(page.locator('[data-test="product-name"]').getByText('Thor Hammer')).toBeVisible();
         await expect(page.locator('img.card-img-top')).toBeVisible();
-        await expect(page).toHaveScreenshot("thor-hammer-product-after-search.png");
+        
+        test.step.skip('skipping screenshot of thor-hammer-product-after-search', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-product-after-search.png")
+        });
 
         await products.getByRole('link').click();
         await expect(page.getByAltText('Thor Hammer')).toBeVisible();
@@ -27,33 +30,49 @@ test.describe('Verify screenshots -> add item to cart and confirm payment', () =
         const productPrice = '11.14';
         await expect(page.locator('#description')).toHaveText(productDescription);
         await expect(page.locator('[data-test="unit-price"]')).toHaveText(productPrice);
-        await expect(page).toHaveScreenshot("thor-hammer-product-details.png");
+        
+        test.step.skip('skipping screenshot of thor-hammer-product-details', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-product-details.png");
+        });
 
         const add_to_cart_button = page.locator('[data-test="add-to-cart"]');
         await expect(add_to_cart_button).toHaveText('Add to cart ');
         await add_to_cart_button.click();
         expect(page.locator('div').filter({ hasText: 'Product added to shopping' }).nth(2)).toBeTruthy();
-        await expect(page).toHaveScreenshot("thor-hammer-add-to-cart.png");
+        
+        test.step.skip('skipping screenshot of thor-hammer-add-to-cart', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-add-to-cart.png")
+        });
         
         await page.waitForTimeout(15000);
         await expect(page.locator('#toast-container')).not.toBeVisible();
         
         await expect(page.locator('span#lblCartCount')).toHaveText('1');
-        await expect(page).toHaveScreenshot("thor-hammer-cart.png");
+
+        test.step.skip('skipping screenshot of thor-hammer-cart', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-cart.png");
+        });
 
         await page.locator('[data-test="nav-cart"]').click();
         await expect(page.locator('span.product-title')).toHaveText('Thor Hammer ');
         await expect(page.locator('input[type=number]')).toHaveValue('1');
         await expect(page.locator('[data-test="product-price"]')).toHaveText('$11.14');
         await expect(page.locator('[data-test="line-price"]')).toHaveText('$11.14');
-        await expect(page).toHaveScreenshot("thor-hammer-incart-cart-screen.png");
+        
+        test.step.skip('skipping screenshot of thor-hammer-incart-cart-screen', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-incart-cart-screen.png")
+        });
 
         const proceedToCheckoutButton = page.locator('[data-test="proceed-1"]');
         await expect(proceedToCheckoutButton).toHaveText('Proceed to checkout');
         await proceedToCheckoutButton.click();
 
         await expect(page.locator('.row p')).toHaveText('Hello Jane Doe, you are already logged in. You can proceed to checkout.');
-        await expect(page).toHaveScreenshot("thor-hammer-incart-signin-screen.png");
+        
+        test.step.skip('skipping screenshot of thor-hammer-incart-signin-screen', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-incart-signin-screen.png");
+        });
+        
         await page.locator('[data-test="proceed-2"]').click();
 
         await page.locator('#address').fill('Test street 100');
@@ -61,15 +80,21 @@ test.describe('Verify screenshots -> add item to cart and confirm payment', () =
         await page.locator('#state').fill('KA');
         await page.locator('#country').fill('Austria');
         await page.locator('#postcode').fill('00000');
-        await expect(page).toHaveScreenshot("thor-hammer-incart-address-screen.png");
+        test.step.skip('skipping screenshot of thor-hammer-incart-address-screen', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-incart-address-screen.png");
+        });
         await page.locator('[data-test="proceed-3"]').click();
 
         const select = page.locator('#payment-method');
-        await select.selectOption({label: 'Cash on Delivery'});
-        await expect(page).toHaveScreenshot("thor-hammer-incart-payment-screen.png");
+        await select.selectOption({ label: 'Cash on Delivery' });
+        test.step.skip('skipping screenshot of thor-hammer-incart-payment-screen', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-incart-payment-screen.png");
+        });
         await page.locator('[data-test="finish"]').click();
         
         await expect(page.locator('.help-block')).toHaveText('Payment was successful');
-        await expect(page).toHaveScreenshot("thor-hammer-incart-payment-success-screen.png");
+        test.step.skip('skipping screenshot of thor-hammer-incart-payment-success-screen', async () => {
+            await expect(page).toHaveScreenshot("thor-hammer-incart-payment-success-screen.png");
+        });
     });
 });
